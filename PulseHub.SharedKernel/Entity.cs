65540255b8bc0a8459a1647f9e994f91dd014611
Szxro <szxro@gmail.com>
2024-08-24 17:31:34 -0400

    Add base entity

---
+++ PulseHub.SharedKernel/Entity.cs
@@ -0,0 +1,12 @@
+﻿using PulseHub.SharedKernel.Contracts;
+
+namespace PulseHub.SharedKernel;
+
+public class Entity : AuditableEntity, ISoftDelete
+{
+    public int Id { get; set; }
+
+    public bool IsDeleted { get; set;    }
+
+    public DateTimeOffset DeletedAtUtc { get; set; }
+}

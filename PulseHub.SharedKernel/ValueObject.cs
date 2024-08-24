2c723686068e209b491ecb8434681e31d06c44d0
Szxro <szxro@gmail.com>
2024-08-24 17:35:05 -0400

    Add value object model

---
+++ PulseHub.SharedKernel/ValueObject.cs
@@ -0,0 +1,49 @@
+﻿namespace PulseHub.SharedKernel;
+
+// https://learn.microsoft.com/en-us/dotnet/architecture/microservices/microservice-ddd-cqrs-patterns/implement-value-objects
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    protected static bool EqualOperator(ValueObject left, ValueObject right)
+    {
+        if (ReferenceEquals(left, null) ^ ReferenceEquals(right, null))
+        {
+            return false;
+        }
+        return ReferenceEquals(left, right) || left!.Equals(right!);
+    }
+
+    protected static bool NotEqualOperator(ValueObject left, ValueObject right)
+    {
+        return !EqualOperator(left, right);
+    }
+
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    #pragma warning disable CS8765
+    public override bool Equals(object obj)
+    {
+        return obj is ValueObject valueObject && GetEqualityComponents().SequenceEqual(valueObject.GetEqualityComponents()); ;
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Select(x => x != null ? x.GetHashCode() : 0)
+            .Aggregate((x, y) => x ^ y);
+    }
+
+    public bool Equals(ValueObject? other)
+    {
+        return other is not null && GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public static bool operator ==(ValueObject one, ValueObject two)
+    {
+        return EqualOperator(one, two);
+    }
+
+    public static bool operator !=(ValueObject one, ValueObject two)
+    {
+        return NotEqualOperator(one, two);
+    }
+}

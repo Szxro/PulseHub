c5146a23008dbaed8a0ebdfc4249fdb995356d9e
Szxro <szxro@gmail.com>
2024-08-24 17:38:07 -0400

    Add access key entity

---
+++ PulseHub.Domain/Entities/AccessKey.cs
@@ -0,0 +1,16 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Entities;
+
+public class AccessKey : Entity
+{
+    public string EncryptedKey { get; set; } = string.Empty;
+
+    public int ApplicationId { get; set; }
+
+    public Application Application { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+
+    public DateTimeOffset LastUsed { get; set; }
+}

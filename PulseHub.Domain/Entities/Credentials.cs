d21c8d38cc33c999717acc6ae8f92449c99399b3
Szxro <szxro@gmail.com>
2024-08-24 17:39:05 -0400

    Add credentials entity

---
+++ PulseHub.Domain/Entities/Credentials.cs
@@ -0,0 +1,19 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Entities;
+
+public class Credentials : Entity
+{
+    public Credentials()
+    {
+        UserCredentials = new HashSet<UserCredentials>();
+    }
+
+    public string HashValue { get; set; } = string.Empty;
+
+    public string SaltValue { get; set; } = string.Empty;
+
+    public bool IsActive { get; set; } = true;
+
+    public ICollection<UserCredentials> UserCredentials { get; set; }
+}

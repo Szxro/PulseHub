cbc8520bd1214ce2f369f8bf3fc0cd3d2cc7e335
Szxro <szxro@gmail.com>
2024-08-24 17:40:30 -0400

    Add email value object

---
+++ PulseHub.Domain/ValueObjects/Email.cs
@@ -0,0 +1,18 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.ValueObjects;
+
+public class Email : ValueObject
+{
+    public string Value { get; }
+
+    public Email(string value)
+    {
+        Value = value;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}

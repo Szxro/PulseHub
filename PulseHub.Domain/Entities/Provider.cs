937a593be10934130213ba868ac55bda75917e00
Szxro <szxro@gmail.com>
2024-08-24 17:39:41 -0400

    Add provider entity

---
+++ PulseHub.Domain/Entities/Provider.cs
@@ -0,0 +1,17 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Entities;
+
+public class Provider : Entity
+{
+    public Provider()
+    {
+        Applications = new HashSet<Application>();
+    }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public ICollection<Application> Applications { get; set; }
+}

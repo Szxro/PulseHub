46c01a14498a8194477e75bd0146cb335ad5c778
Szxro <szxro@gmail.com>
2024-08-24 17:38:39 -0400

    Add application entity

---
+++ PulseHub.Domain/Entities/Application.cs
@@ -0,0 +1,27 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Entities;
+
+public class Application : Entity
+{
+    public Application()
+    {
+        AccessKeys = new HashSet<AccessKey>();
+    }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string ProviderApplicationId { get; set; } = string.Empty;
+
+    public int UserId { get; set; }
+
+    public User User { get; set; } = null!;
+
+    public int ProviderId { get; set; }
+
+    public Provider Provider { get; set; } = null!;
+
+    public ICollection<AccessKey> AccessKeys { get; set; }
+}

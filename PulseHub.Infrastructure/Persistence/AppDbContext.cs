61cdc92f3a0b1e436d544605be59960162694ed4
Szxro <szxrocode@gmail.com>
2024-08-25 16:34:02 -0400

    Add db context

---
+++ PulseHub.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PulseHub.Domain.Entities;
+
+namespace PulseHub.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<DateTime>().HaveColumnType("datetime2");
+
+        base.ConfigureConventions(configurationBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<User> User => Set<User>();
+
+    public DbSet<Credentials> Credentials => Set<Credentials>();
+
+    public DbSet<UserCredentials> UserCredentials => Set<UserCredentials>();
+
+    public DbSet<Application> Application => Set<Application>();
+
+    public DbSet<Provider> Provider => Set<Provider>();
+
+    public DbSet<AccessKey> AccessKey => Set<AccessKey>();
+}

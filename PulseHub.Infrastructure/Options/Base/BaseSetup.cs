8209f87bfb793324ab3834920a7d83a3390f0bdb
Szxro <szxrocode@gmail.com>
2024-08-25 18:23:08 -0400

    Add base setup class for the configurable classes

---
+++ PulseHub.Infrastructure/Options/Base/BaseSetup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using PulseHub.SharedKernel.Contracts;
+
+namespace PulseHub.Infrastructure.Options.Base;
+
+public abstract class BaseSetup<TOption> : IConfigureOptions<TOption>
+    where TOption : class, IOptionSetup
+{
+    private readonly IConfiguration _configuration;
+
+    public BaseSetup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void Configure(TOption options)
+    {
+        _configuration
+            .GetSection(options.SectionName)
+            .Bind(options);
+    }
+}

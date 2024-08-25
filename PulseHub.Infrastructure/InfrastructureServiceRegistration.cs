465779c913810b3e2adaa860eb2ba6e51a29211a
Szxro <szxrocode@gmail.com>
2024-08-25 18:30:39 -0400

    Add extension method to apply the specify services of the infrastructure layer

--- PulseHub.Infrastructure/InfrastructureServiceRegistration.cs
+++ PulseHub.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,6 +1,37 @@
-﻿namespace PulseHub.Infrastructure;
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using PulseHub.Infrastructure.Extensions;
+using PulseHub.Infrastructure.Options.Database;
+using PulseHub.Infrastructure.Persistence;
+
+namespace PulseHub.Infrastructure;
 
 public static class InfrastructureServiceRegistration
 {
+    public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(typeof(InfrastructureServiceRegistration).Assembly);
 
+        services.AddConfigurableOptions();
+
+        services.AddDbContext<AppDbContext>((provider,options) => 
+        {
+            DatabaseOptions databaseOptions = provider.GetRequiredService<IOptions<DatabaseOptions>>().Value;
+
+            options.UseSqlServer(databaseOptions.ConnectionString,sqlOptions =>
+            {
+                sqlOptions.CommandTimeout(databaseOptions.CommandTimeout);
+
+            })
+            .UseSnakeCaseNamingConvention();
+
+            // Enable it in development
+            options.EnableDetailedErrors(databaseOptions.EnableDetailedErrors);
+            options.EnableSensitiveDataLogging(databaseOptions.EnableSensitiveDataLogging);
+        });
+
+        return services;
+    }
 }

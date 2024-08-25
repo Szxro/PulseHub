c75787a3f007f3eecf299421ae43806e7c77a7fd
Szxro <szxrocode@gmail.com>
2024-08-25 18:28:06 -0400

    Add option extension method

---
+++ PulseHub.Infrastructure/Extensions/OptionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using PulseHub.Infrastructure.Options.Database;
+using PulseHub.Infrastructure.Validators;
+using PulseHub.SharedKernel.Contracts;
+
+namespace PulseHub.Infrastructure.Extensions;
+
+public static partial class InfrastructureExtensions
+{
+    public static IServiceCollection AddConfigurableOptions(this IServiceCollection services)
+    {
+        services.ConfigureOptions<DatabaseOptionsSetup>()
+                .AddFluentValidator<DatabaseOptions>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddFluentValidator<TOption>(this IServiceCollection services)
+        where TOption : class,IOptionSetup
+    {
+        services.AddSingleton<IValidateOptions<TOption>>(
+            provider => new FluentOptionsValidator<TOption>(provider.GetRequiredService<IServiceScopeFactory>()));
+
+        return services;
+    }
+}

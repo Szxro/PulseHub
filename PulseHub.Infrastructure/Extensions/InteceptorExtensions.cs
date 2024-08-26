294d2aae55c0485689e48f4691f09da4fa0fd314
Sebastian <svargas@isf.do>
2024-08-26 10:45:18 -0400

    Add auditable intercerptor and extension method for the interceptors

---
+++ PulseHub.Infrastructure/Extensions/InteceptorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PulseHub.Infrastructure.Persistence.Interceptors;
+
+namespace PulseHub.Infrastructure.Extensions;
+
+public static partial class InfrastructureExtensions
+{
+    public static IServiceCollection AddInterceptors(this IServiceCollection services)
+    {
+        services.AddSingleton<AuditableEntityInterceptor>();
+
+        return services;
+    }
+}
---
+++ PulseHub.Infrastructure/Persistence/Interceptors/AuditableEntityInterceptor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using PulseHub.SharedKernel;
+
+namespace PulseHub.Infrastructure.Persistence.Interceptors;
+
+public class AuditableEntityInterceptor : SaveChangesInterceptor
+{
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData,
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = default)
+    {
+        if (eventData.Context is not null)
+        {
+            UpdateAuditableEntities(eventData.Context);
+        }
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private void UpdateAuditableEntities(DbContext dbContext)
+    {
+        List<EntityEntry<AuditableEntity>> entities = dbContext.ChangeTracker.Entries<AuditableEntity>()
+                                                                             .Where(entity => entity.State == EntityState.Added || entity.State == EntityState.Modified)
+                                                                             .ToList();
+
+        foreach (EntityEntry<AuditableEntity> entity in entities)
+        {
+            if (entity.State == EntityState.Added)
+            {
+                SetCurrentProperty(entity, nameof(AuditableEntity.CreatedAtUtc), DateTimeOffset.UtcNow);
+                SetCurrentProperty(entity, nameof(AuditableEntity.ModifiedAtUtc), DateTimeOffset.UtcNow);
+            }
+
+            if (entity.State == EntityState.Modified)
+            {
+                SetCurrentProperty(entity, nameof(AuditableEntity.ModifiedAtUtc), DateTimeOffset.UtcNow);
+            }
+        }
+    }
+
+    private void SetCurrentProperty(
+        EntityEntry entity,
+        string propertyName,
+        DateTimeOffset dateTime) => entity.Property(propertyName).CurrentValue = dateTime; 
+}

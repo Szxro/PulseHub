b44fd5f0c61ec896b0a9ce55b572771974edaf80
Sebastian <svargas@isf.do>
2024-08-26 08:49:21 -0400

    Add application layer and user secrets

--- PulseHub.Api/Program.cs
+++ PulseHub.Api/Program.cs
@@ -1,10 +1,13 @@
+using PulseHub.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     // Add services to the container.
-
+    builder.Services.AddInfrastructureLayer();
     builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
+    builder.Configuration.AddUserSecrets<Program>(optional:false,reloadOnChange:true);
 }
 
 

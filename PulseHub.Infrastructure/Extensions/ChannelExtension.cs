a0a854e8e37fcdb5138c170535ca55471dd4604a
Sebastian <svargas@isf.do>
2024-08-26 15:47:39 -0400

    Add extension method for the channels

---
+++ PulseHub.Infrastructure/Extensions/ChannelExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PulseHub.Infrastructure.Channels;
+
+namespace PulseHub.Infrastructure.Extensions;
+
+public static partial class InfrastructureExtensions
+{
+    public static IServiceCollection AddChannels(this IServiceCollection services)
+    {
+        // Need to be singleton to maintain state
+        services.AddSingleton<DomainEventChannel>();
+
+        return services;
+    }
+}

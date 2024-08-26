98d3b86b75b2786fcdd43308b95d94ef9547dcfb
Sebastian <svargas@isf.do>
2024-08-26 14:21:32 -0400

    Add extension method for capture exceptions of multiple tasks

---
+++ PulseHub.SharedKernel/Extensions/TaskExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace PulseHub.SharedKernel.Extensions;
+
+public static class TaskExtensions
+{
+    public static async Task<TResult[]> WhenAll<TResult>(this IEnumerable<Task<TResult>> source)
+    {
+        Task<TResult[]> task = Task.WhenAll(source);
+
+        try
+        {
+            return await task.ConfigureAwait(false);
+
+        } catch
+        {
+            if (task.Exception is not null) ExceptionDispatchInfo.Capture(task.Exception).Throw();
+
+            throw;
+        }
+    }
+}

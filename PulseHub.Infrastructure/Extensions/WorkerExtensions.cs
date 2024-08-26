10165cdba240a26eae51bf5646f95939ad63646b
Sebastian <svargas@isf.do>
2024-08-26 10:43:54 -0400

    Add extension method for the workers

---
+++ PulseHub.Infrastructure/Extensions/WorkerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PulseHub.Infrastructure.Workers;
+
+namespace PulseHub.Infrastructure.Extensions;
+
+public static partial class InfrastructureExtensions
+{
+    public static IServiceCollection AddWorkers(this IServiceCollection services)
+    {
+        services.AddHostedService<DatabaseInitliazerWorker>();
+
+        return services;
+    }
+}

71672ef4c1b28b92b5926569565a7716a8863387
Sebastian <svargas@isf.do>
2024-08-26 14:23:14 -0400

    Configure fluent validators and mediatr behaviors and services

---
+++ PulseHub.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using PulseHub.Application.Common.Behaviors;
+
+namespace PulseHub.Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(typeof(ApplicationServiceRegistration).Assembly);
+
+        services.AddMediatR(options =>
+        {
+            options.RegisterServicesFromAssembly(typeof(ApplicationServiceRegistration).Assembly);
+
+            // Pipelines Behaviors
+            options.AddOpenBehavior(typeof(RequestValidationPipelineBehavior<,>));
+            options.AddOpenBehavior(typeof(RequestLoggingPipelineBehavior<,>));
+            options.AddOpenBehavior(typeof(RequestPerformancePipelineBehavior<,>));
+            options.AddOpenBehavior(typeof(RequestExceptionHandlingPipelineBehavior<,>));
+        });
+
+        return services;
+    }
+}

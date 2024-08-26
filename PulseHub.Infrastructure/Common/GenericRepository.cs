905e5fe1d661205870910a3b12b492974a9d1188
Sebastian <svargas@isf.do>
2024-08-26 10:39:13 -0400

    Add unit of work and generic repository

---
+++ PulseHub.Infrastructure/Common/GenericRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using PulseHub.Infrastructure.Persistence;
+using PulseHub.SharedKernel.Contracts;
+using System.Linq.Expressions;
+
+namespace PulseHub.Infrastructure.Common;
+
+public abstract class GenericRepository<TEntity>
+    where TEntity : class, IEntity
+{
+    private readonly AppDbContext _dbContext;
+
+    protected GenericRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void Add(TEntity entity)
+    {
+        _dbContext.Set<TEntity>().Add(entity);
+    }
+
+    public void AddRange(IEnumerable<TEntity> entities)
+    {
+        _dbContext.Set<TEntity>().AddRange(entities);
+    }
+
+    public void Remove(TEntity entity)
+    {
+        _dbContext.Set<TEntity>().Remove(entity);
+    }
+
+    public void RemoveRange(IEnumerable<TEntity> entities)
+    {
+        _dbContext.Set<TEntity>().RemoveRange(entities);
+    }
+
+    public void Update(TEntity entity)
+    {
+        _dbContext.Set<TEntity>().Update(entity);
+    }
+
+    public void UpdateRange(IEnumerable<TEntity> entities)
+    {
+        _dbContext.Set<TEntity>().UpdateRange(entities);
+    }
+
+    public async Task<TEntity?> GetById(int id)
+    {
+        return await _dbContext.Set<TEntity>().Where(x => x.Id == id).FirstOrDefaultAsync();
+    }
+
+    public async Task<int?> BulkDelete(Expression<Func<TEntity,bool>> filter)
+    {
+        return await _dbContext.Set<TEntity>().Where(filter).ExecuteDeleteAsync();
+    }
+
+    public async Task<int?> BulkUpdate(
+        Expression<Func<TEntity, bool>> filter,
+        Expression<Func<SetPropertyCalls<TEntity>, SetPropertyCalls<TEntity>>> properties)
+    {
+        return await _dbContext.Set<TEntity>().Where(filter).ExecuteUpdateAsync(properties);
+    }
+}
---
+++ PulseHub.Infrastructure/Common/UnitOfWork.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
+using PulseHub.Domain.Contracts;
+using PulseHub.Infrastructure.Persistence;
+using System.Data;
+
+namespace PulseHub.Infrastructure.Common;
+
+public sealed class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _dbContext;
+    private readonly ILogger<UnitOfWork> _logger;
+
+    public UnitOfWork(
+        AppDbContext dbContext,
+        ILogger<UnitOfWork> logger)
+    {
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+
+    public async Task<IDbTransaction> BeginTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            IDbContextTransaction transaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
+
+            return transaction.GetDbTransaction();
+
+        } catch (Exception ex)
+        {
+            _logger.LogError("An exception happen trying to get a DbTransaction with the error message : {message}",ex.Message);
+
+            throw;
+        }
+    }
+
+    public void ChangeTrackerToUnchanged(object entity)
+    {
+        _dbContext.Entry(entity).State = EntityState.Unchanged;
+    }
+
+    public async Task<int?> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            return await _dbContext.SaveChangesAsync(cancellationToken);
+
+        } catch (Exception ex)
+        {
+            _logger.LogError(
+                "An exception happen trying to save changes into the database {provider} with the error message : {message}",
+                _dbContext.Provider,
+                ex.Message);
+
+            throw;
+        }
+    }
+}

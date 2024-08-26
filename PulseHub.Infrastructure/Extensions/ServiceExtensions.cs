3998f5c8add60d7314836f04120255f42dc61749
Sebastian <svargas@isf.do>
2024-08-26 10:41:10 -0400

    Add extension method for the services

---
+++ PulseHub.Infrastructure/Extensions/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PulseHub.Domain.Contracts;
+using PulseHub.Infrastructure.Common;
+using PulseHub.Infrastructure.Services;
+
+namespace PulseHub.Infrastructure.Extensions;
+
+public static partial class InfrastructureExtensions
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        services.AddScoped<IDatabaseInitializerService, DatabaseInitializerService>();
+       
+        return services;
+    }
+}

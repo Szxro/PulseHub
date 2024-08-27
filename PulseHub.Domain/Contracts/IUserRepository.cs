23f204797f6206ca8c18c36069b51eaeac641070
Sebastian <svargas@isf.do>
2024-08-27 15:43:28 -0400

    Add user repository interface

---
+++ PulseHub.Domain/Contracts/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using PulseHub.Domain.Entities;
+using PulseHub.Domain.ValueObjects;
+
+namespace PulseHub.Domain.Contracts;
+
+public interface IUserRepository : IRepositoryWriter<User>
+{
+    Task<bool> IsEmailUnique(Email email);
+
+    Task<bool> IsUserNameUnique(string username);
+}

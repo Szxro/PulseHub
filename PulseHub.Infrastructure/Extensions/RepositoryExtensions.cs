dbcd50a2b5ce2b7791e8d5343b34d568f2098951
Sebastian <svargas@isf.do>
2024-08-27 15:44:58 -0400

    Add extension method to register repositories into the DI container

---
+++ PulseHub.Infrastructure/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PulseHub.Domain.Contracts;
+using PulseHub.Infrastructure.Repositories;
+
+namespace PulseHub.Infrastructure.Extensions;
+
+public static partial class InfrastructureExtensions
+{
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        return services;
+    }
+}

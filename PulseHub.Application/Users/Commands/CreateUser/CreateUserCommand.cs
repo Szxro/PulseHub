c597c40af4b9817772386080b69de39998149248
Sebastian <svargas@isf.do>
2024-08-27 15:47:39 -0400

    Add create user use case,validator and event handler for the add user event

---
+++ PulseHub.Application/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,75 @@
+﻿using PulseHub.Domain.Contracts;
+using PulseHub.Domain.Entities;
+using PulseHub.Domain.Events.User;
+using PulseHub.Domain.ValueObjects;
+using PulseHub.SharedKernel;
+using PulseHub.SharedKernel.Contracts;
+
+namespace PulseHub.Application.Users.Commands.CreateUser;
+
+public record CreateUserCommand(
+    string firstName,
+    string lastName,
+    string username,
+    string email,
+    string password) : ICommand<Result>;
+
+public class CreateUserCommandHandler : ICommandHandler<CreateUserCommand, Result>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IHashingService _hashingService;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateUserCommandHandler(
+        IUserRepository userRepository,
+        IHashingService hashingService,
+        IUnitOfWork unitOfWork)
+    {
+        _userRepository = userRepository;
+        _hashingService = hashingService;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        if (!Email.IsValid(request.email,out Result<Email>? validEmail))
+        {
+            return Result.Failure(Error.ValidationFailure("Invalid email address. Please check and try again."));
+        }
+
+        if (await _userRepository.IsEmailUnique(validEmail!.Value!))
+        {
+            return Result.Failure(Error.ValidationFailure($"Email address '{validEmail.Value!.Value}' is already registered."));
+        }
+
+        if (await _userRepository.IsUserNameUnique(request.username))
+        {
+            return Result.Failure(Error.ValidationFailure($"Username '{request.username}' is already registered."));
+        }
+
+        string hash = _hashingService.GetHash(request.password, out byte[] salt);
+
+        User newUser = new User
+        {
+            FirstName = request.firstName,
+            Lastname = request.lastName,
+            Email = validEmail.Value!,
+            Username = request.username
+        };
+
+        newUser.AddCredentials(
+            new Credentials
+            {
+                HashValue = hash,
+                SaltValue = Convert.ToHexString(salt),
+            });
+
+        newUser.AddEvent(new AddUserEvent(newUser.Username,newUser.Email.Value));
+
+        _userRepository.Add(newUser);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success;
+    }
+}
---
+++ PulseHub.Application/Users/Commands/CreateUser/CreateUserCommandValidator.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+
+namespace PulseHub.Application.Users.Commands.CreateUser;
+
+public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+{
+    public CreateUserCommandValidator()
+    {
+        RuleFor(command => command.email)
+            .NotNull().WithMessage("The email address can't be null.")
+            .NotEmpty().WithMessage("The email address can't be empty.");
+
+        RuleFor(command => command.password)
+            .NotNull().WithMessage("The password can't be null.")
+            .NotEmpty().WithMessage("The password can't be empty.")
+            .MinimumLength(8).WithMessage("The password must be at least 8 characters long.")
+            .Matches(@"[A-Z]").WithMessage("The password must contain at least one uppercase letter.")
+            .Matches(@"[a-z]").WithMessage("The password must contain at least one lowercase letter.")
+            .Matches(@"[0-9]").WithMessage("The password must contain at least one number.")
+            .Matches(@"[\W_]").WithMessage("The password must contain at least one special character.");
+
+        RuleFor(command => command.username)
+            .NotNull().WithMessage("The username can't be null")
+            .NotEmpty().WithMessage("The username can't be empty")
+            .MinimumLength(3).WithMessage("The username must contain at least three characters.")
+            .MaximumLength(20).WithMessage("The username must not exceed 20 characters.")
+            .Matches(@"^[a-zA-Z0-9_]+$").WithMessage("The username can only contain letters, numbers, and underscores."); ;
+
+        RuleFor(command => command.firstName)
+            .NotNull().WithMessage("The firstname can't be null.")
+            .NotEmpty().WithMessage("The firstname can't be empty.")
+            .MinimumLength(1).WithMessage("The firstname must contain at least one characters.");
+
+        RuleFor(command => command.lastName)
+            .NotNull().WithMessage("The firstname can't be null.")
+            .NotEmpty().WithMessage("The firstname can't be empty.")
+            .MinimumLength(1).WithMessage("The firstname must contain at least one characters.");
+    }
+}

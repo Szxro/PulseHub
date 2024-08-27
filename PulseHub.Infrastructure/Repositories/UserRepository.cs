0eebecc8632b38a782f46a35f13e3fd7309f16b1
Sebastian <svargas@isf.do>
2024-08-27 15:44:02 -0400

    Add user repository logic

---
+++ PulseHub.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PulseHub.Domain.Contracts;
+using PulseHub.Domain.Entities;
+using PulseHub.Domain.ValueObjects;
+using PulseHub.Infrastructure.Common;
+using PulseHub.Infrastructure.Persistence;
+
+namespace PulseHub.Infrastructure.Repositories;
+
+public class UserRepository : GenericRepository<User>, IUserRepository
+{
+    public UserRepository(AppDbContext dbContext) : base(dbContext) { }
+
+    public async Task<bool> IsEmailUnique(Email email)
+    {
+        return await _dbContext.User.AsNoTracking().AnyAsync(user => user.Email.Value == email.Value);
+    }
+
+    public async Task<bool> IsUserNameUnique(string username)
+    {
+        return await _dbContext.User.AsNoTracking().AnyAsync(user => user.Username == username);
+    }
+}

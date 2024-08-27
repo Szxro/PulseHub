7fa5ffa0dc529c656cb6ac808b30c4f192896a68
Sebastian <svargas@isf.do>
2024-08-27 15:34:53 -0400

    Refactor the validations of the database options

--- PulseHub.Infrastructure/Options/Database/Validations/DatabaseOptionsValidations.cs
+++ PulseHub.Infrastructure/Options/Database/Validations/DatabaseOptionsValidations.cs
@@ -6,22 +6,21 @@ public class DatabaseOptionsValidations : AbstractValidator<DatabaseOptions>
 {
     public DatabaseOptionsValidations()
     {
-        RuleFor(x => x.ConnectionString).NotNull().WithMessage("The {PropertyName} can't be null");
+        RuleFor(x => x.ConnectionString)
+            .NotNull().WithMessage("The {PropertyName} can't be null")
+            .NotEmpty().WithMessage("The {PropertyName} can't be empty");
 
-        RuleFor(x => x.ConnectionString).NotEmpty().WithMessage("The {PropertyName} can't be empty");
+        RuleFor(x => x.CommandTimeout)
+            .NotNull().WithMessage("The {PropertyName} can't be null")
+            .NotEmpty().WithMessage("The {PropertyName} can't be empty")
+            .GreaterThan(0).WithMessage("The {PropertyName} must be greather than 0");
 
-        RuleFor(x => x.CommandTimeout).NotNull().WithMessage("The {PropertyName} can't be null");
+        RuleFor(x => x.EnableDetailedErrors)
+            .NotNull().WithMessage("The {PropertyName} can't be null")
+            .NotEmpty().WithMessage("The {PropertyName} can't be empty");
 
-        RuleFor(x => x.CommandTimeout).NotEmpty().WithMessage("The {PropertyName} can't be empty");
-
-        RuleFor(x => x.CommandTimeout).Must(timeout => timeout > 0).WithMessage("The {PropertyName} must be greather than 0");
-
-        RuleFor(x => x.EnableDetailedErrors).NotNull().WithMessage("The {PropertyName} can't be null");
-
-        RuleFor(x => x.EnableDetailedErrors).NotEmpty().WithMessage("The {PropertyName} can't be empty");
-
-        RuleFor(x => x.EnableSensitiveDataLogging).NotNull().WithMessage("The {PropertyName} can't be null");
-
-        RuleFor(x => x.EnableSensitiveDataLogging).NotEmpty().WithMessage("The {PropertyName} can't be empty");
+        RuleFor(x => x.EnableSensitiveDataLogging)
+            .NotNull().WithMessage("The {PropertyName} can't be null")
+            .NotEmpty().WithMessage("The {PropertyName} can't be empty");
     }
 }

1460c0d7b990756ed90d44185e07987fcf9e931f
Sebastian <svargas@isf.do>
2024-08-27 15:40:48 -0400

    Add event for when a new user is add to the system

---
+++ PulseHub.Domain/Events/User/AddUserEvent.cs
@@ -0,0 +1,16 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Events.User;
+
+public class AddUserEvent : DomainEvent
+{
+    public AddUserEvent(string username,string email)
+    {
+        Username = username;
+        Email = email;
+    }
+
+    public string Username { get; set; }
+
+    public string Email { get; set; }
+}

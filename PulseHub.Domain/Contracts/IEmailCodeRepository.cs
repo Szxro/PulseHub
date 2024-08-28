0a55e6c5d46c53c623c2a4cbdafee8803e94550e
Sebastian <svargas@isf.do>
2024-08-28 13:58:56 -0400

    Add email code repository interface and logic

---
+++ PulseHub.Domain/Contracts/IEmailCodeRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace PulseHub.Domain.Contracts;
+
+public interface IEmailCodeRepository
+{
+    Task<string?> GetEmailCodeByUsernameAndEmail(string username, string email, CancellationToken cancellationToken = default);
+}
---
+++ PulseHub.Infrastructure/Repositories/EmailCodeRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PulseHub.Domain.Contracts;
+using PulseHub.Domain.Entities;
+using PulseHub.Infrastructure.Common;
+using PulseHub.Infrastructure.Persistence;
+
+namespace PulseHub.Infrastructure.Repositories;
+
+public class EmailCodeRepository : GenericRepository<EmailCode>, IEmailCodeRepository
+{
+    public EmailCodeRepository(AppDbContext dbContext) : base(dbContext) { }
+
+    public async Task<string?> GetEmailCodeByUsernameAndEmail(string username, string email,CancellationToken cancellationToken = default)
+    {
+        return await _dbContext.EmailCode.AsNoTracking()
+                                         .Include(x => x.User)
+                                         .Where(x => x.User.Username == username && x.User.Email.Value == email)
+                                         .Select(x => x.Code)
+                                         .FirstOrDefaultAsync(cancellationToken);   
+    }
+}

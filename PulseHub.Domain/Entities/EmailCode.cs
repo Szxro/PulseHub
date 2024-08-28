141b61b2625e4ff4ba2154b8eb6dea32b98e6176
Sebastian <svargas@isf.do>
2024-08-28 08:29:10 -0400

    Add email code entity

---
+++ PulseHub.Domain/Entities/EmailCode.cs
@@ -0,0 +1,18 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Entities;
+
+public class EmailCode : Entity
+{
+    public string Code { get; set; } = string.Empty;
+
+    public bool IsInvalid { get; set; }
+
+    public bool IsVerified { get; set; }
+
+    public bool IsExpired { get; set; }
+
+    public int UserId { get; set; }
+
+    public User? User { get; set; } 
+}

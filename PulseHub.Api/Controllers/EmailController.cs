2e1e08ae18114d14c1b6dc703bebd029c152ccb8
Sebastian <svargas@isf.do>
2024-08-28 15:27:52 -0400

    Add endpoint to verify the email code

---
+++ PulseHub.Api/Controllers/EmailController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PulseHub.Application.EmailCodes.Commands.VerifyEmailCode;
+using PulseHub.SharedKernel;
+
+namespace PulseHub.Api.Controllers
+{
+    [Route("api/email")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public EmailController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost("verify")]
+
+        public async Task<ActionResult<Result>> VerifyEmailCode([FromQuery] string code)
+        {
+            Result response = await _sender.Send(new VerifyEmailCodeCommand(code));
+
+            return response.IsSuccess ? Ok(response) : BadRequest(response);
+        }
+    }
+}

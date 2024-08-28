9c356f60cbcbd1d971c43abe4aea65a61ba3a5b6
Sebastian <svargas@isf.do>
2024-08-28 15:27:23 -0400

    Add event and event handler to send a welcome message to the verify user

---
+++ PulseHub.Application/EmailCodes/EventHandlers/SendWelcomeEmailEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using PulseHub.Domain.Contracts;
+using PulseHub.Domain.Events.EmailCode;
+using PulseHub.SharedKernel.Contracts;
+using PulseHub.SharedKernel;
+using PulseHub.SharedKernel.Templates;
+
+namespace PulseHub.Application.EmailCodes.EventHandlers;
+
+internal class SendWelcomeEmailEventHandler : IDomainEventHandler<SendWelcomeEmailEvent>
+{
+    private readonly IEmailService _emailService;
+    private readonly ILogger<SendWelcomeEmailEventHandler> _logger;
+
+    public SendWelcomeEmailEventHandler(
+        IEmailService emailService,
+        ILogger<SendWelcomeEmailEventHandler> logger)
+    {
+        _emailService = emailService;
+        _logger = logger;
+    }
+    public async Task Handle(SendWelcomeEmailEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation(
+            "Sending a welcome message to the user with the usernam '{username}' and email '{email}'",
+            notification.Username,
+            notification.Email);
+
+        await _emailService.SendEmailAsync(
+            new EmailMessage
+            {
+                ToAddress = notification.Email,
+                Subject = "Welcome to PulseHub",
+                Body = EmailTemplates.GetWelcomeEmailBodyHtml(notification.Username)
+            }, cancellationToken);
+    }
+}
---
+++ PulseHub.Domain/Events/EmailCode/SendWelcomeEmailEvent.cs
@@ -0,0 +1,16 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Events.EmailCode;
+
+public class SendWelcomeEmailEvent : DomainEvent
+{
+    public SendWelcomeEmailEvent(string username,string email)
+    {
+        Username = username;
+        Email = email;
+    }
+
+    public string Username { get; } = string.Empty;
+
+    public string Email { get; } = string.Empty;
+}

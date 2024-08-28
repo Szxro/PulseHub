420398b86115370078d6c0eb9f1617bf522003ac
Sebastian <svargas@isf.do>
2024-08-28 13:52:29 -0400

    Add email message class and email templates

---
+++ PulseHub.SharedKernel/EmailMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace PulseHub.SharedKernel;
+
+public class EmailMessage
+{
+    public string ToAddress { get; set; } = string.Empty;
+
+    public string Subject { get; set; } = string.Empty;
+
+    public string Body { get; set; } = string.Empty;
+}
---
+++ PulseHub.SharedKernel/Templates/EmailTemplates.cs
@@ -0,0 +1,18 @@
+﻿namespace PulseHub.SharedKernel.Templates;
+
+public static class EmailTemplates
+{
+    public static string GetVerificationEmailBodyHtml(string username,string emailCode)
+    {
+        return $@"
+                 <html>
+                    <body>
+                        <p>Hello: '{username}',</p>
+                        <p>Please use the following verification code to complete your registration:</p>
+                        <h2>{emailCode}</h2>
+                        <p>If you did not request this verification, please ignore this email.</p>
+                        <p>Thank you!</p>
+                    </body>
+                 </html>";
+    }
+}

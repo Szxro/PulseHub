2674b004a9e2be988ef0a50cadfffc12f8004eee
Sebastian <svargas@isf.do>
2024-08-28 15:23:22 -0400

    Add access key service interface and implementation

---
+++ PulseHub.Domain/Contracts/IAccessKeyService.cs
@@ -0,0 +1,6 @@
+﻿namespace PulseHub.Domain.Contracts;
+
+public interface IAccessKeyService
+{
+    string GenerateUniqueAccessKey(int length = 10);
+}
---
+++ PulseHub.Infrastructure/Services/AccessKeyService.cs
@@ -0,0 +1,34 @@
+﻿using PulseHub.Domain.Contracts;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PulseHub.Infrastructure.Services;
+
+public class AccessKeyService : IAccessKeyService
+{
+    private static readonly char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+
+    public string GenerateUniqueAccessKey(int length = 10)
+    {
+        byte[] buffer = new byte[4 * length];
+
+        using RandomNumberGenerator rng = RandomNumberGenerator.Create();
+
+        // Fill the array of bytes with random sequence of values
+        rng.GetBytes(buffer);
+
+        StringBuilder builder = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++)
+        {
+            // Getting a random uint from the buffer at a specify position
+            uint randomNumber = BitConverter.ToUInt32(buffer, i * 4);
+
+            long index = randomNumber % chars.Length; // calculating the index 
+
+            builder.Append(chars[index]);
+        }
+
+        return builder.ToString();
+    }
+}

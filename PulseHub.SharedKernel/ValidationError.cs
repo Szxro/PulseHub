63f1a8968ebb7ae6b01ab670a64ba0ff5cdf63cd
Sebastian <svargas@isf.do>
2024-08-30 16:01:01 -0400

    Add validation error class

---
+++ PulseHub.SharedKernel/ValidationError.cs
@@ -0,0 +1,16 @@
+﻿namespace PulseHub.SharedKernel;
+using PulseHub.SharedKernel.Enums;
+
+public class ValidationError : Error
+{
+    public ValidationError(Error[] errors)
+        : base(string.Empty,
+               "Validation.Error",
+               "One or more validation errors occurred.",
+               ErrorType.Validation)
+    {
+        Errors = errors;
+    }
+
+    public Error[] Errors { get; set; }
+}

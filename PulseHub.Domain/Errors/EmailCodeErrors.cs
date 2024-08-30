9a64eaba292c2e81d7ec373a1a3fb5dca610826a
Sebastian <svargas@isf.do>
2024-08-30 15:52:56 -0400

    Add entities domain errors

---
+++ PulseHub.Domain/Errors/EmailCodeErrors.cs
@@ -0,0 +1,22 @@
+﻿using PulseHub.Domain.Entities;
+using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Errors;
+
+public static class EmailCodeErrors
+{
+    public static Error EmailCodeNotFound(string code)
+        => Error.NotFound($"The email code '{code}' was not found.");
+
+    public static Error EmailCodeAlreadyVerified(string code)
+        => Error.Validation(string.Empty, "EmailCodeVerified.Error", $"The email code '{code}' is already verify.");
+
+    public static Error EmailCodeAlreadyInvalid(string code)
+        => Error.Validation(string.Empty, "EmailCodeInvalid.Error", $"The email code '{code}' is already invalid.");
+
+    public static Error EmailCodeAlreadyExpired(string code)
+        => Error.Validation(string.Empty, "EmailCodeExpired.Error", $"The email code '{code}' is already expired.");
+
+    public static Error UserAlreadyVerified(string username, string email)
+        => Error.Validation(string.Empty, "UserVerified.Error", $"The current user with the username '{username}' and email '{email}' is already verified");
+}
---
+++ PulseHub.Domain/Errors/UserErrors.cs
@@ -0,0 +1,15 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Errors;
+
+public static class UserErrors
+{
+    public static Error EmailNotUnique(string email)
+        => Error.Conflit($"Email address '{email}' is already registered.");
+
+    public static Error UsernameNotUnique(string username)
+        => Error.Conflit($"Username '{username}' is already registered");
+
+    public static Error UserNotFoundByUsernameAndEmail(string username, string email)
+        => Error.NotFound($"User not found with the username '{username}' and email '{email}'");
+}

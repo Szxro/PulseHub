5da2332e95b978713f075eb9dba235de6347fb2d
Sebastian <svargas@isf.do>
2024-09-02 15:50:59 -0400

    Add event handler and event to send a notification about the lock out

---
+++ PulseHub.Application/Users/EventHandlers/UserLockEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using PulseHub.Domain.Contracts;
+using PulseHub.Domain.Events.Users;
+using PulseHub.SharedKernel.Contracts;
+using PulseHub.SharedKernel;
+using PulseHub.SharedKernel.Templates;
+using Microsoft.Extensions.Logging;
+
+namespace PulseHub.Application.Users.EventHandlers;
+
+public class UserLockEventHandler : IDomainEventHandler<UserLockOutEvent>
+{
+    private readonly IEmailService _emailService;
+    private readonly ILogger<UserLockOutEvent> _logger;
+
+    public UserLockEventHandler(
+        IEmailService emailService,
+        ILogger<UserLockOutEvent> logger)
+    {
+        _emailService = emailService;
+        _logger = logger;
+    }
+    public async Task Handle(UserLockOutEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogWarning(
+            "Sending a lockout notification for a user with the username '{username}' and with the email '{email}'",
+            notification.Username,
+            notification.Email);
+
+        await _emailService.SendEmailAsync(
+            new EmailMessage
+            {
+                Subject = "Your Account Has Been Temporarily Locked",
+                Body = EmailTemplates.GetLockOutEmailBodyHtml(notification.Username,DateTime.Now),
+                ToAddress = notification.Email
+            });
+    }
+}
---
+++ PulseHub.Domain/Events/Users/UserLockOutEvent.cs
@@ -0,0 +1,16 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Events.Users;
+
+public class UserLockOutEvent : DomainEvent
+{
+    public UserLockOutEvent(string username,string email)
+    {
+        Username = username;
+        Email = email;  
+    }
+
+    public string Username { get; set; }
+
+    public string Email { get; set; }
+}

da7fd7182fe3372615d5d710e2ae797b98e53c31
Sebastian <svargas@isf.do>
2024-09-02 15:45:50 -0400

    Add refresh token repository and logic

---
+++ PulseHub.Domain/Contracts/IRefreshTokenRepository.cs
@@ -0,0 +1,8 @@
+﻿using PulseHub.Domain.Entities;
+
+namespace PulseHub.Domain.Contracts;
+
+public interface IRefreshTokenRepository : IRepositoryWriter<RefreshToken>
+{
+    Task<RefreshToken?> GetUnusedUserRefreshTokenByUsernameAsync(string username,CancellationToken cancellationToken = default);
+}
---
+++ PulseHub.Infrastructure/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using PulseHub.Domain.Contracts;
+using PulseHub.Domain.Entities;
+using PulseHub.Infrastructure.Common;
+using PulseHub.Infrastructure.Persistence;
+
+namespace PulseHub.Infrastructure.Repositories;
+
+public class RefreshTokenRepository : GenericRepository<RefreshToken>, IRefreshTokenRepository
+{
+    public RefreshTokenRepository(AppDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public async Task<RefreshToken?> GetUnusedUserRefreshTokenByUsernameAsync(string username, CancellationToken cancellationToken = default)
+    {
+        return await _dbContext.RefreshToken.Include(x => x.User).Where(x => x.User.Username == username && !x.IsUsed && !x.IsRevoked).FirstOrDefaultAsync(cancellationToken);
+    }
+}

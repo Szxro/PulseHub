0426c2ccc5b8e1745b64f0062a45cea58c97c9c2
Sebastian <svargas@isf.do>
2024-09-02 15:47:47 -0400

    Add new errors for the user entity and add errors for the credentials entity

---
+++ PulseHub.Domain/Errors/CredentialsErrors.cs
@@ -0,0 +1,12 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Errors;
+
+public static class CredentialsErrors
+{
+    public static Error IncorrectCredentials(int attempts)
+        => Error.Validation(string.Empty,"IncorrectCredentials.Error", $"The provided credentials are incorrect,retry count {attempts}/3");
+
+    public static Error InvalidCredentials
+        => Error.Validation(string.Empty, "InvalidCredentials.Error", "The provided credentials are invalid, contact the tecnical support to fix the issue.");
+}
--- PulseHub.Domain/Errors/UserErrors.cs
+++ PulseHub.Domain/Errors/UserErrors.cs
@@ -12,4 +12,25 @@ public static class UserErrors
 
     public static Error UserNotFoundByUsernameAndEmail(string username, string email)
         => Error.NotFound($"User not found with the username '{username}' and email '{email}'");
+
+    public static Error UserNotFoundByUsername(string username)
+        => Error.NotFound($"User not found with the username '{username}'.");
+
+    public static Error UserLockOut(string username, DateTime dateTime)
+    {
+        TimeSpan timeRemaining = dateTime - DateTime.Now;
+
+        return Error.Validation(string.Empty,"UserLockOut.Error", $"The User with the username '{username}' is lock out, lock out end in {FormatTimeRemaining(timeRemaining)}");
+    }
+
+    private static string FormatTimeRemaining(TimeSpan timeSpan)
+    {
+        return timeSpan switch
+        {
+            { TotalDays: > 1} => $"{Math.Floor(timeSpan.TotalDays)} days",
+            { TotalHours: > 1 } => $"{Math.Floor(timeSpan.TotalHours)} hours",
+            { TotalMinutes: > 1 } => $"{Math.Floor(timeSpan.TotalMinutes)} minutes",
+            _ => $"{(int)timeSpan.TotalSeconds} seconds"
+        };
+    }
 }

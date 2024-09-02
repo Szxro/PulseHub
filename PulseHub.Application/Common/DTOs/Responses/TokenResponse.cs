50b3672fbeb00f4c785567c186a9e53293cf61bb
Sebastian <svargas@isf.do>
2024-09-02 15:28:57 -0400

    Add token response class

---
+++ PulseHub.Application/Common/DTOs/Responses/TokenResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace PulseHub.Application.Common.DTOs.Responses;
+
+public class TokenResponse
+{
+    public string Token { get; set; } = string.Empty;
+
+    public string RefreshedToken { get; set; } = string.Empty;
+}

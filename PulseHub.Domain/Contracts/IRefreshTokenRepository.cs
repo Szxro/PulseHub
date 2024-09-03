b3f589643022c077089d1ccda9f79236fe1b9cfe
Sebastian <svargas@isf.do>
2024-09-03 09:38:14 -0400

    Add method to retrieve expired refresh tokens

--- PulseHub.Domain/Contracts/IRefreshTokenRepository.cs
+++ PulseHub.Domain/Contracts/IRefreshTokenRepository.cs
@@ -5,4 +5,6 @@ namespace PulseHub.Domain.Contracts;
 public interface IRefreshTokenRepository : IRepositoryWriter<RefreshToken>
 {
     Task<RefreshToken?> GetUnusedUserRefreshTokenByUsernameAsync(string username,CancellationToken cancellationToken = default);
+
+    Task<List<RefreshToken>> GetExpiredUserRefreshTokenAsync(DateTime currentDateTime, CancellationToken cancellationToken = default);
 }
--- PulseHub.Infrastructure/Repositories/RefreshTokenRepository.cs
+++ PulseHub.Infrastructure/Repositories/RefreshTokenRepository.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
 using PulseHub.Domain.Contracts;
 using PulseHub.Domain.Entities;
 using PulseHub.Infrastructure.Common;
@@ -9,12 +8,17 @@ namespace PulseHub.Infrastructure.Repositories;
 
 public class RefreshTokenRepository : GenericRepository<RefreshToken>, IRefreshTokenRepository
 {
-    public RefreshTokenRepository(AppDbContext dbContext) : base(dbContext)
+    public RefreshTokenRepository(AppDbContext dbContext) : base(dbContext) { }
+
+    public async Task<List<RefreshToken>> GetExpiredUserRefreshTokenAsync(DateTime currentDateTime,CancellationToken cancellationToken = default)
     {
+        return await _dbContext.RefreshToken.Where(x => !x.IsUsed && !x.IsRevoked && !x.IsExpired && x.ExpirationDate <= currentDateTime).ToListAsync(cancellationToken);
     }
 
     public async Task<RefreshToken?> GetUnusedUserRefreshTokenByUsernameAsync(string username, CancellationToken cancellationToken = default)
     {
-        return await _dbContext.RefreshToken.Include(x => x.User).Where(x => x.User.Username == username && !x.IsUsed && !x.IsRevoked).FirstOrDefaultAsync(cancellationToken);
+        return await _dbContext.RefreshToken.Include(x => x.User)
+                                            .Where(x => x.User.Username == username && !x.IsUsed && !x.IsRevoked && !x.IsExpired)
+                                            .FirstOrDefaultAsync(cancellationToken);
     }
 }

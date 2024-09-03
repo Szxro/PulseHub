1aa45cdb8b0266c050c7589dba85d6736f3c109c
Sebastian <svargas@isf.do>
2024-09-03 13:58:13 -0400

    Add domain errors for the refresh token and token entity

---
+++ PulseHub.Domain/Errors/RefreshTokenErrors.cs
@@ -0,0 +1,9 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Errors;
+
+public static class RefreshTokenErrors
+{
+    public static Error InvalidRefreshToken
+        => Error.Validation(string.Empty, "InvalidRefreshToken.Error", "The provided refresh token is invalid or expired.");
+}
---
+++ PulseHub.Domain/Errors/TokenErrors.cs
@@ -0,0 +1,18 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Errors;
+
+public static class TokenErrors
+{
+    public static Error InvalidToken
+        => Error.Validation(string.Empty, "InvalidToken.Error", "The provided token is invalid.");
+
+    public static Error InvalidTokenClaims 
+        => Error.Validation(string.Empty, "InvalidTokenClaims.Error", "Token does not contain a valid username or expire time stamp.");
+
+    public static Error InvalidTokenExpiry
+        => Error.Validation(string.Empty, "InvalidTokenExpiry.Error", "Token expiration timestamp is invalid.");
+
+    public static Error TokenStillValid
+        => Error.Validation(string.Empty, "TokenStillValid.Error", "The provided token is still valid.");
+}

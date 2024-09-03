effc31a7863c3045335396ffcf65d4051dcadce2
Sebastian <svargas@isf.do>
2024-09-03 09:41:28 -0400

    Add strategy extension method

---
+++ PulseHub.Infrastructure/Extensions/StrategyExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PulseHub.Domain.Contracts;
+using PulseHub.Domain.Entities;
+using PulseHub.Infrastructure.Strategies;
+using PulseHub.Infrastructure.Strategies.Context;
+
+namespace PulseHub.Infrastructure.Extensions;
+
+public static partial class InfrastructureExtensions
+{
+    public static IServiceCollection AddStrategies(this IServiceCollection services)
+    {
+        // Context
+        services.AddScoped<ExpirationStrategyContext<EmailCode>>();
+
+        services.AddScoped<ExpirationStrategyContext<RefreshToken>>();
+
+        // Strategies
+        services.AddScoped<IExpiredStrategy<EmailCode>, EmailCodeExpirationStrategy>();
+
+        services.AddScoped<IExpiredStrategy<RefreshToken>, RefreshTokenExpirationStrategy>();
+
+        return services;
+    }
+}

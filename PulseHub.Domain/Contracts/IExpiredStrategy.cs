4cfabff7fd43218278bafc6dcc60fce0d7e14f59
Sebastian <svargas@isf.do>
2024-09-03 09:35:29 -0400

    Add expiration strategy interface for email code and refresh token entities

---
+++ PulseHub.Domain/Contracts/IExpiredStrategy.cs
@@ -0,0 +1,11 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Contracts;
+
+public interface IExpiredStrategy<T>
+    where T : Entity
+{
+    Task<List<T>> GetExpiredEntitiesAsync(DateTime currentDateTime,CancellationToken cancellationToken = default);
+
+    void MarkEntitiesAsExpired(List<T> entities);
+}

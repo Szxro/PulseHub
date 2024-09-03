e4f55f8a4087f6d96a43654a07fe7e4d60dad2da
Sebastian <svargas@isf.do>
2024-09-03 14:04:56 -0400

    Add refresh token controller to regenerate new tokens and refresh tokens

---
+++ PulseHub.Api/Controllers/RefreshTokenController.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using MediatR;
+
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+using PulseHub.Api.Common;
+using PulseHub.Api.Extensions;
+using PulseHub.Application.Common.DTOs.Requests.RefreshToken;
+using PulseHub.Application.Common.DTOs.Responses;
+using PulseHub.Application.RefreshTokens.Commands.RegenerateToken;
+using PulseHub.SharedKernel;
+
+namespace PulseHub.Api.Controllers
+{
+    [Route("api/refresh-token")]
+    [ApiController]
+    public class RefreshTokenController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public RefreshTokenController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost("regenerate")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IResult> RegenerateToken(RegenerateTokenRequest tokenResponse)
+        {
+            RegenerateTokenCommand command = new RegenerateTokenCommand(tokenResponse.token,tokenResponse.refreshToken);
+
+            Result<TokenResponse> result = await _sender.Send(command);
+
+            return result.Match(
+                onSuccess: () => CustomResult.Success(result),
+                onFailure: CustomResult.Problem);
+        }
+    }
+}

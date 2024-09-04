ffe6202029a13215b190d1727c98ce3af6b8328d
Sebastian <svargas@isf.do>
2024-09-04 15:55:06 -0400

    Add core module into the app.module and clean app.component.ts

--- PulseHub.WebApp/src/app/app.component.ts
+++ PulseHub.WebApp/src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'PulseHub.WebApp';
-}
+export class AppComponent{}

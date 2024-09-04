7b694499bff80741316626e4cc96329c63e424a3
Sebastian <svargas@isf.do>
2024-09-04 09:52:23 -0400

    Add configurations for the access key and application entity

---
+++ PulseHub.Infrastructure/Persistence/Configurations/AccessKeyConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using PulseHub.Domain.Entities;
+
+namespace PulseHub.Infrastructure.Persistence.Configurations;
+
+public class AccessKeyConfiguration : IEntityTypeConfiguration<AccessKey>
+{
+    public void Configure(EntityTypeBuilder<AccessKey> builder)
+    {
+        builder.HasIndex(x => x.EncryptedKey).IsUnique();
+    }
+}
---
+++ PulseHub.Infrastructure/Persistence/Configurations/ApplicationConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using DomainApplication = PulseHub.Domain.Entities.Application;
+
+namespace PulseHub.Infrastructure.Persistence.Configurations;
+
+public class ApplicationConfiguration : IEntityTypeConfiguration<DomainApplication>
+{
+    public void Configure(EntityTypeBuilder<DomainApplication> builder)
+    {
+        builder.HasIndex(x => x.Name).IsUnique();
+    }
+}

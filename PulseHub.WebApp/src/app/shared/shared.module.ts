73488e44b8ed4cc0b8f2423efa89976b7f5fb4e2
Sebastian <svargas@isf.do>
2024-09-19 15:11:20 -0400

    Declare and exports directive and components

--- PulseHub.WebApp/src/app/shared/shared.module.ts
+++ PulseHub.WebApp/src/app/shared/shared.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ErrorMessageDirective } from './directives/error-message.directive';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ErrorMessageDirective,
+    NotFoundComponent,
+    SpinnerComponent
+  ],
+  exports:[
+    ErrorMessageDirective,
+    NotFoundComponent,
+    SpinnerComponent
+  ],
   imports: [
     CommonModule
   ]

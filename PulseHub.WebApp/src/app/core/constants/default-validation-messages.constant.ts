0d13fc522d6ddb3a943d7e41f69389fa7dad1e8a
Sebastian <svargas@isf.do>
2024-09-19 15:01:21 -0400

    Add default validation messages

---
+++ PulseHub.WebApp/src/app/core/constants/default-validation-messages.constant.ts
@@ -0,0 +1,10 @@
+import { ErrorHandler } from "../models/handlers/error-handler.model";
+
+const DEFAULT_VALIDATION_MESSAGES: ErrorHandler = {
+    field:{
+        "required": () => 'Field is required',
+        "minlength": ({ requiredLength }) => `The field a least need to be ${requiredLength} characters long`
+    }
+}
+
+export { DEFAULT_VALIDATION_MESSAGES }

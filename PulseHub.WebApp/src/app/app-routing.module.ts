942249888baeb0d9f06d025c7d6e3c3dccfbc3ad
Sebastian <svargas@isf.do>
2024-09-19 15:13:31 -0400

    Define routes for the application

--- PulseHub.WebApp/src/app/app-routing.module.ts
+++ PulseHub.WebApp/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 const routes: Routes = [
   {
-    path:"",
-    loadChildren:() => import("./features/auth/auth.module").then(m => m.AuthModule)
+    path:'',
+    pathMatch:'full',
+    redirectTo:'/auth/login'
+  },
+  {
+    path:'home',
+    loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule),
+    // TODO: GUARD THE HOME ROUTE FOR UNAUTHORIZED USERS
+  },
+  {
+    path:'auth',
+    loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path:'**',
+    component:NotFoundComponent
   }
 ];
 

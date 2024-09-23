addcbdc96c4486196ee4445798bb7dabb5bc2b03
Sebastian <svargas@isf.do>
2024-09-23 13:44:39 -0400

    Add email error message

--- PulseHub.WebApp/src/app/features/auth/constants/auth-validation-messages.constant.ts
+++ PulseHub.WebApp/src/app/features/auth/constants/auth-validation-messages.constant.ts
@@ -9,6 +9,7 @@ export const AUTH_VALIDATION_MESSAGES: ErrorHandler = {
     field:{
         ...DEFAULT_VALIDATION_MESSAGES.field,
         weakPassword: () => "The password at least need to have one uppercase, lowercase, number and special character",
-        invalidUsername: () => "The username can only contain letters, numbers, and underscores"
+        invalidUsername: () => "The username can only contain letters, numbers, and underscores",
+        email:() => "The provide email need to be a valid email"
     }
 }

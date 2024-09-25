1dc893491520dbf093425a6ae83a11bb83f767da
Sebastian <svargas@isf.do>
2024-09-25 08:59:06 -0400

    Add functionality to react to the 401 and 403 status code when using the token

--- PulseHub.WebApp/src/app/core/interceptors/token.interceptor.ts
+++ PulseHub.WebApp/src/app/core/interceptors/token.interceptor.ts
@@ -1,12 +1,18 @@
-import { HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { catchError, Observable, switchMap, throwError } from 'rxjs';
 import { LocalStorageService } from '../services/local-storage.service';
+import { AuthService } from '../services/auth.service';
+import { ToastService } from '../services/toast.service';
 import { TokenResponse } from '../models/responses/token-response';
 
 
 export function tokenInterceptor(req:HttpRequest<unknown>,next: HttpHandlerFn):Observable<HttpEvent<unknown>>{
   const storageService = inject(LocalStorageService);
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const toastService = inject(ToastService);
   
   const token = storageService.getItemByKey<TokenResponse>('token');
   
@@ -17,5 +23,38 @@ export function tokenInterceptor(req:HttpRequest<unknown>,next: HttpHandlerFn):O
       },
     });
   }
-  return next(req);
+  return next(req).pipe(
+    catchError((error) =>{
+      if(error instanceof HttpErrorResponse && token !== null){
+        switch(error.status){
+          case 401:
+            return authService.regenerateToken(token).pipe(
+              switchMap((response) =>{
+                storageService.setItemByKey('token',response.data);
+
+                const newReq = req.clone({
+                  setHeaders:{
+                    'Authorization' : `Bearer ${response.data.token}`
+                  }
+                });
+
+                return next(newReq);
+              }),
+              catchError((error) =>{
+                storageService.purge();
+                router.navigateByUrl('/auth/login');
+                toastService.error({ message:'Session expired. Please log in again.' });
+
+                return throwError(() => error);
+              })
+            )
+          case 403:
+            toastService.error({ message: 'Access forbidden. You do not have permission to access this resource.' });
+            router.navigateByUrl('/home');
+            break;
+        }
+      }
+      return throwError(() => error);
+    })
+  );
 }

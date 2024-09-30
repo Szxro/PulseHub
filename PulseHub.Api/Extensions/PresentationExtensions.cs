4585b12a3d4f0fa350776e7bb2a5e8354b76ce11
Sebastian <svargas@isf.do>
2024-09-30 14:36:27 -0400

    Remove JWT auth scheme as default and add the access key scheme into the authentication middleware

--- PulseHub.Api/Extensions/PresentationExtensions.cs
+++ PulseHub.Api/Extensions/PresentationExtensions.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-
-using PulseHub.Api.Middlewares;
+﻿using PulseHub.Api.Middlewares;
 
 namespace PulseHub.Api.Extensions;
 
@@ -8,7 +6,7 @@ public static partial class ApiExtensions
 {
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+        services.AddAuthentication().AddAccessKeyAuth().AddJwtBearer();
         services.AddAuthorization();
 
         services.AddEndpointsApiExplorer();

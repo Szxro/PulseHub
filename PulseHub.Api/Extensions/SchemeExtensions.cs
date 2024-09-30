39134f72eabf5b2cef7ef5a5d03e234fbaa016a6
Sebastian <svargas@isf.do>
2024-09-30 14:35:10 -0400

    Add extension method to add the access key custom auth scheme

---
+++ PulseHub.Api/Extensions/SchemeExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Primitives;
+using PulseHub.Api.Scheme.Defaults;
+using PulseHub.Api.Scheme.Handler;
+using PulseHub.Api.Scheme.Options;
+using PulseHub.Api.Scheme.Events;
+
+namespace PulseHub.Api.Extensions;
+
+public static partial class ApiExtensions
+{
+    public static AuthenticationBuilder AddAccessKeyAuth(this AuthenticationBuilder builder)
+    {
+        builder.AddScheme<AccessKeyAuthenticationSchemeOptions, AccessKeyAuthenticationHandler>(AccessKeyDefaults.AuthenticationScheme, options => 
+        {
+            options.Events = new AccessKeyEvents
+            {
+                OnHeaderReceivedContext = context =>
+                {
+                    if (context.HttpContext.Request.Headers.TryGetValue("AccessKey",out StringValues accessKey) 
+                        && context.HttpContext.Request.Headers.TryGetValue("ApplicationName", out StringValues applicationName))
+                    {
+                        context.AccessKey = accessKey;
+                        context.ApplicationName = applicationName;
+                    }
+
+                    return Task.CompletedTask;
+                }
+            };
+        });
+
+        return builder; 
+    }
+}

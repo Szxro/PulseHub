e765eeb06c78a6b55372e25be81912e2d5a358f9
Sebastian <svargas@isf.do>
2024-09-30 08:48:54 -0400

    Add unique index for the provider application id located in the application entity

---
+++ PulseHub.Infrastructure/Persistence/Migrations/20240930123554_Add_UniqueIndex_ProviderApplicationId.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PulseHub.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_UniqueIndex_ProviderApplicationId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "provider_application_id",
+                table: "application",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_application_provider_application_id",
+                table: "application",
+                column: "provider_application_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_application_provider_application_id",
+                table: "application");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "provider_application_id",
+                table: "application",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}

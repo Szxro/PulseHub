8ff7a1c7459c3da3ec21ecd847b4dbfafd4f6a63
Sebastian <svargas@isf.do>
2024-09-30 09:05:31 -0400

    Add command and validator for the creation of an application

---
+++ PulseHub.Application/Applications/Commands/CreateApplication/CreateApplicationCommand.cs
@@ -0,0 +1,97 @@
+﻿using PulseHub.Application.Common.DTOs.Responses;
+using PulseHub.Domain.Contracts;
+using PulseHub.Domain.Entities;
+using PulseHub.Domain.Errors;
+using PulseHub.SharedKernel;
+using PulseHub.SharedKernel.Contracts;
+using DomainApplication = PulseHub.Domain.Entities.Application;
+
+namespace PulseHub.Application.Applications.Commands.CreateApplication;
+
+public record CreateApplicationCommand(
+    string name,
+    string description,
+    string provider,
+    string providerApplicationId) : ICommand<ApplicationCreatedResponse>;
+
+public class CreateApplicationCommandHandler : ICommandHandler<CreateApplicationCommand, ApplicationCreatedResponse>
+{
+    private readonly IApplicationRepository _applicationRepository;
+    private readonly IUserRepository _userRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IProviderRepository _providerRepository;
+    private readonly ICurrentUserService _currentUserService;
+    private readonly IAccessKeyService _accessKeyService;
+
+    public CreateApplicationCommandHandler(
+        IApplicationRepository applicationRepository,
+        IUserRepository userRepository,
+        IUnitOfWork unitOfWork,
+        IProviderRepository providerRepository,
+        ICurrentUserService currentUserService,
+        IAccessKeyService accessKeyService)
+    {
+        _applicationRepository = applicationRepository;
+        _userRepository = userRepository;
+        _unitOfWork = unitOfWork;
+        _providerRepository = providerRepository;
+        _currentUserService = currentUserService;
+        _accessKeyService = accessKeyService;
+    }
+
+    public async Task<Result<ApplicationCreatedResponse>> Handle(CreateApplicationCommand request, CancellationToken cancellationToken)
+    {
+        if (await _applicationRepository.IsApplicationNameNotUnique(request.name))
+        {
+            return Result<ApplicationCreatedResponse>.Failure(ApplicationErrors.ApplicationNameNotUnique(request.name));
+        }
+
+        (string? username,string? email) = _currentUserService.GetCurrentUser();
+
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(email))
+        {
+            return Result<ApplicationCreatedResponse>.Failure(Error.NotFound("The current user was not found!!"));
+        }
+
+        User? foundUser = await _userRepository.GetUserByUsernameAndEmailAsync(username, email);
+
+        if (foundUser is null)
+        {
+            return Result<ApplicationCreatedResponse>.Failure(UserErrors.UserNotFoundByUsernameAndEmail(username, email));
+        }
+
+        Provider? foundProvider = await _providerRepository.GetProviderByNameAsync(request.provider);
+
+        if (foundProvider is null)
+        {
+            return Result<ApplicationCreatedResponse>.Failure(ProviderErrors.ProviderNotFoundByName(request.provider));
+        }
+
+        DomainApplication newApplication = new DomainApplication
+        {
+            Name = request.name,
+            Description = request.description,
+            User = foundUser,
+            Provider = foundProvider,
+            ProviderApplicationId = request.providerApplicationId,
+        };
+
+        string newAccessKey = _accessKeyService.GenerateUniqueAccessKey();
+
+        newApplication.AccessKeys.Add(new AccessKey()
+        {
+            EncryptedKey = _accessKeyService.EncryptAccessKey(newAccessKey),
+            IsActive = true,
+        });
+
+        _unitOfWork.ChangeTrackerToUnchanged(newApplication.User);
+
+        _unitOfWork.ChangeTrackerToUnchanged(newApplication.Provider);
+
+        _applicationRepository.Add(newApplication);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result<ApplicationCreatedResponse>.Success(new ApplicationCreatedResponse(request.name, newAccessKey));
+    }
+}
---
+++ PulseHub.Application/Applications/Commands/CreateApplication/CreateApplicationCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+
+namespace PulseHub.Application.Applications.Commands.CreateApplication;
+
+public class CreateApplicationCommandValidator : AbstractValidator<CreateApplicationCommand>
+{
+    public CreateApplicationCommandValidator()
+    {
+        RuleFor(x => x.name)
+            .NotEmpty().WithMessage("The application name can't be empty.")
+            .NotNull().WithMessage("The application name can't be null.")
+            .MinimumLength(3).WithMessage("The application name must contain at least three characters.")
+            .MaximumLength(20).WithMessage("The application name must not exceed 20 characters.")
+            .Matches(@"^[a-zA-Z0-9_]+$").WithMessage("The application name can only contain letters, numbers, and underscores.");
+
+        RuleFor(x => x.description)
+            .NotEmpty().WithMessage("The application description can't be empty.")
+            .NotNull().WithMessage("The application description can't be null.")
+            .MinimumLength(15).WithMessage("The application description must contain at least fifteen characters.")
+            .MaximumLength(35).WithMessage("The application description must not exceed 35 characters.");
+
+        RuleFor(x => x.provider)
+            .NotEmpty().WithMessage("The provider can't be empty.")
+            .NotNull().WithMessage("The provider description can't be null.");
+
+        RuleFor(x => x.providerApplicationId)
+            .NotEmpty().WithMessage("The application provider application id can't be empty.")
+            .NotNull().WithMessage("The application provider application id can't be null.");
+    }
+}

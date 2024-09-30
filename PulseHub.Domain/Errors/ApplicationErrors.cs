b402c59098d9a8b22f2c17217adc7fc24587860a
Sebastian <svargas@isf.do>
2024-09-30 08:55:15 -0400

    Add application and provider entities errors

---
+++ PulseHub.Domain/Errors/ApplicationErrors.cs
@@ -0,0 +1,9 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Errors;
+
+public class ApplicationErrors
+{
+    public static Error ApplicationNameNotUnique(string name)
+        => Error.Conflit($"An Application with the name '{name}' is already registered.");
+}
---
+++ PulseHub.Domain/Errors/ProviderErrors.cs
@@ -0,0 +1,9 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Errors;
+
+public class ProviderErrors
+{
+    public static Error ProviderNotFoundByName(string providerName)
+        => Error.NotFound($"The provider with the name {providerName} was not found");
+}

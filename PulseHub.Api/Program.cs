35f7bcec3c2a67823c4f359cce0a1f5a2ca53b66
Sebastian <svargas@isf.do>
2024-10-01 10:04:10 -0400

    Add serilog into the host and add serilog settings

--- PulseHub.Api/Program.cs
+++ PulseHub.Api/Program.cs
@@ -1,9 +1,13 @@
 using PulseHub.Api.Extensions;
 using PulseHub.Application;
 using PulseHub.Infrastructure;
+using Serilog;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 {
+    // Adding Serilog
+    builder.Host.UseSerilog((host, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(host.Configuration));
+
     // Add services to the container.
     builder.Services
             .AddPresentation()
@@ -22,6 +26,8 @@ WebApplication app = builder.Build();
         app.UseSwagger();
         app.UseSwaggerUI();
     }
+
+    app.UseSerilogRequestLogging();
 
     app.UseHttpsRedirection();
 

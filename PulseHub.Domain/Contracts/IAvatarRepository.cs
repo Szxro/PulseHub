3ffe4934eb5f02e21c370e4e14bf1b86adce324d
Sebastian <svargas@isf.do>
2024-10-07 11:07:26 -0400

    Add avatar repository interface and logic

---
+++ PulseHub.Domain/Contracts/IAvatarRepository.cs
@@ -0,0 +1,8 @@
+﻿using PulseHub.Domain.Entities;
+
+namespace PulseHub.Domain.Contracts;
+
+public interface IAvatarRepository : IRepositoryWriter<Avatar>
+{
+    Task<Avatar?> GetUserAvatarByUsernameAsync(string username, CancellationToken cancellationToken = default);
+}
---
+++ PulseHub.Infrastructure/Repositories/AvatarRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PulseHub.Domain.Contracts;
+using PulseHub.Domain.Entities;
+using PulseHub.Infrastructure.Common;
+using PulseHub.Infrastructure.Persistence;
+
+namespace PulseHub.Infrastructure.Repositories;
+
+public sealed class AvatarRepository : GenericRepository<Avatar>, IAvatarRepository
+{
+    public AvatarRepository(AppDbContext dbContext) : base(dbContext) { }
+
+    public async Task<Avatar?> GetUserAvatarByUsernameAsync(string username, CancellationToken cancellationToken = default)
+    {
+        return await _dbContext.Avatar.Include(x => x.User)
+                                      .Where(x => x.User!.Username == username)
+                                      .FirstOrDefaultAsync(cancellationToken);
+    }
+}

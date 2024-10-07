7bf0599c9d65f12e5e6a937206932f15f3dcd1f7
Sebastian <svargas@isf.do>
2024-10-07 11:10:23 -0400

    Add avatar controller and upload endpoint

---
+++ PulseHub.Api/Controllers/AvatarController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PulseHub.Api.Common;
+using PulseHub.Api.Extensions;
+using PulseHub.Application.Avatars.Commands.CreateAvatar;
+using PulseHub.SharedKernel;
+
+namespace PulseHub.Api.Controllers;
+
+[Route("api/avatar")]
+[ApiController]
+public class AvatarController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public AvatarController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpPost("upload")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public async Task<IResult> UploadAvatar(IFormFile formFile)
+    {
+        Result result = await _sender.Send(new CreateAvatarCommand(formFile));
+
+        return result.Match(
+            onSuccess: () => Results.Created(),
+            onFailure: CustomResult.Problem);
+    }
+}

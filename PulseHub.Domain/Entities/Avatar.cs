81550ee914f42df73e1289a26f259e3e1c0f49df
Sebastian <svargas@isf.do>
2024-10-07 09:12:26 -0400

    Add avatar entity

---
+++ PulseHub.Domain/Entities/Avatar.cs
@@ -0,0 +1,22 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Entities;
+
+public class Avatar : Entity
+{
+    public string FileName { get; set; } = string.Empty;
+
+    public string Path { get; set; } = string.Empty;
+
+    public long TotalSize { get; set; }
+
+    public int Height { get; set; }
+
+    public int Width { get; set; }
+
+    public string Format { get; set; } = string.Empty;
+
+    public int UserId { get; set; }
+
+    public User? User { get; set; }
+}

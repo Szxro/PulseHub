a7433f07d557759e81d1bf1a7039f2aea9b29b46
Sebastian <svargas@isf.do>
2024-10-09 11:13:55 -0400

    Add connection tracker hub filter to track the connection and rename logging hub filter and add aditional functionality to it

---
+++ PulseHub.Api/Filters/ConnectionTrackerHubFilter.cs
@@ -0,0 +1,79 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.SignalR;
+using PulseHub.Domain.Contracts;
+
+namespace PulseHub.Api.Filters;
+
+public class ConnectionTrackerHubFilter : IHubFilter
+{
+    private readonly ILogger<ConnectionTrackerHubFilter> _logger;
+    private readonly IUserConnectionTrackerService _trackerService;
+
+    public ConnectionTrackerHubFilter(
+        ILogger<ConnectionTrackerHubFilter> logger,
+        IUserConnectionTrackerService trackerService)
+    {
+        _logger = logger;
+        _trackerService = trackerService;
+    }
+
+    public Task OnConnectedAsync(HubLifetimeContext context, Func<HubLifetimeContext, Task> next)
+    {
+        Task response = next(context);
+
+        string? currentUser = context.Context.User?.Claims.Where(x => x.Type == ClaimTypes.Name).FirstOrDefault()?.Value;
+
+        string? currentConnectionId = context.Context.ConnectionId;
+
+        if (string.IsNullOrEmpty(currentUser) || string.IsNullOrEmpty(currentConnectionId))
+        {
+            _logger.LogWarning("The current user or connection Id is missing. User: {User}, Connection ID: {ConnectionId}",
+                              currentUser ?? "Unknown",
+                              currentConnectionId ?? "Unknown");
+
+            return response;
+        }
+
+        _trackerService.AddConnection(currentUser,currentConnectionId);
+
+        _logger.LogInformation("New connection added to the storage with the user {User} and connectionId {connectionId}",
+                               currentUser,
+                               currentConnectionId);
+
+        return response;
+    }
+
+    public Task OnDisconnectedAsync(HubLifetimeContext context, Exception? exception, Func<HubLifetimeContext, Exception?, Task> next)
+    {
+        Task response = next(context, exception);
+
+        string? currentUser = context.Context.User?.Claims.Where(x => x.Type == ClaimTypes.Name).FirstOrDefault()?.Value;
+
+        string? currentConnectionId = context.Context.ConnectionId;
+
+        if (string.IsNullOrEmpty(currentUser) || string.IsNullOrEmpty(currentConnectionId))
+        {
+            _logger.LogWarning("The current user or connection Id is missing. User: {User}, Connection ID: {ConnectionId}",
+                              currentUser ?? "Unknown",
+                              currentConnectionId ?? "Unknown");
+
+            return response;
+        }
+
+        if (exception is not null)
+        {
+            _logger.LogWarning(
+                "The current connection was possibly dropped due to an exception for the user: {user} with the error message: {message}",
+                currentUser,
+                exception.Message);
+        }
+
+        _trackerService.RemoveConnectionByUser(currentUser);
+
+        _logger.LogInformation("The connection was remove from the storage with the user {User} and connectionId {connectionId}",
+                               currentUser,
+                               currentConnectionId);
+
+        return response;
+    }
+}
---
+++ PulseHub.Api/Filters/InvocationLoggingHubFilter.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using System.Security.Claims;
+using Microsoft.AspNetCore.SignalR;
+
+namespace PulseHub.Api.Filters;
+
+public class InvocationLoggingHubFilter : IHubFilter
+{
+    public async ValueTask<object?> InvokeMethodAsync(
+        HubInvocationContext invocationContext,
+        Func<HubInvocationContext, ValueTask<object?>> next)
+    {
+        ILogger<InvocationLoggingHubFilter> logger = invocationContext.ServiceProvider.GetRequiredService<ILogger<InvocationLoggingHubFilter>>();
+
+        string currentUser = invocationContext.Context.User?.Claims.Where(x => x.Type == ClaimTypes.Name).FirstOrDefault()?.Value ?? "Unknown user";
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        try
+        {
+            logger.LogInformation("The user {username} is calling a hub method {hubMethod} with arguments {arguments}",
+                                   currentUser,
+                                   invocationContext.HubMethodName,
+                                   invocationContext.HubMethodArguments);
+
+            return await next(invocationContext);
+
+        }
+        catch (Exception ex)
+        {
+            logger.LogError("An exception ocurred when the user {username} was calling {hubMethodName} with the error message {message}",
+                             currentUser,
+                             invocationContext.HubMethodName,
+                             ex.Message);
+            throw;
+        }
+        finally
+        {
+            stopwatch.Stop();
+
+            long elapsedTime = stopwatch.ElapsedMilliseconds;
+
+            logger.LogInformation(
+                "The current hub method {hubMethod} invoke by the user {user} completed in {elapsedTime}ms",
+                invocationContext.HubMethodName,
+                currentUser,
+                elapsedTime);
+        }
+    }
+}

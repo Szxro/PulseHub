dbdcd72a5a1d93e12d5d7a305c7e1c60c156e014
Szxro <szxro@gmail.com>
2025-01-21 15:30:55 -0400

    Add errors for the current user

---
+++ PulseHub.Domain/Errors/CurrentUserErrors.cs
@@ -0,0 +1,9 @@
+﻿using PulseHub.SharedKernel;
+
+namespace PulseHub.Domain.Errors;
+
+public class CurrentUserErrors
+{
+    public static Error CurrentUserNotFound
+        => Error.NotFound("The current user was not found!!");
+}
